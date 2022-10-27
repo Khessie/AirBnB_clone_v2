67ce70fb1c8ee24d40c8f8ddf8a1828a47caf195
FrankDonny <arkohfrank1@gmail.com>
2022-10-27 09:28:27 +0000

    0x03. AirBnB clone - Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """create archive module"""
+from genericpath import isdir
 from fabric.api import local
 from datetime import datetime
 
@@ -8,7 +9,8 @@ def do_pack():
     """function to zip files"""
     new_date = datetime.now()
     new_date = new_date.strftime('%Y%m%d%H%M%S')
-    local('mkdir versions')
+    if isdir('versions') is False:
+        local('mkdir versions')
     print(f"Packing web_static to versions/web_static_{new_date}.tgz")
     var = local(f'tar -cvzf versions/web_static_{new_date}.tgz web_static')
 
