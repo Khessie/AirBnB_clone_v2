87653e980ee2655f0419514b66a92afd520a93ad
FrankDonny <arkohfrank1@gmail.com>
2022-10-27 09:23:04 +0000

    0x03. AirBnB clone - Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,8 @@ def do_pack():
     """function to zip files"""
     new_date = datetime.now()
     new_date = new_date.strftime('%Y%m%d%H%M%S')
-    local('mkdir -p versions')
+    local('mkdir versions')
+    print(f"Packing web_static to versions/web_static_{new_date}.tgz")
     var = local(f'tar -cvzf versions/web_static_{new_date}.tgz web_static')
 
     if var.succeeded:
