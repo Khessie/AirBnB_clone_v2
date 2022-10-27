96f80938b571e5346022f28829c330220e426799
FrankDonny <arkohfrank1@gmail.com>
2022-10-27 01:53:33 +0000

    0x03. AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 echo '<h1>hello world, this is a test</h1>' | sudo tee /data/web_static/releases/test/index.html
