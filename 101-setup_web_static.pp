f75add15847d7283736651f13c0def928fcf23bf
FrankDonny <arkohfrank1@gmail.com>
2022-10-27 12:57:52 +0000

    0x03. AirBnB clone - Deploy static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,22 +3,22 @@ package {'nginx':
 }
 
 exec {'create_dir':
-  command  => "sudo mkdir -p /data/web_static/releases/test/",
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
   provider => shell
 }
 
 exec {'create_index':
-  command  => "echo '<h1>hello world, this is a test</h1>' | sudo tee /data/web_static/releases/test/index.html",
+  command  => 'echo \'<h1>hello world, this is a test</h1>\' | sudo tee /data/web_static/releases/test/index.html',
   provider => shell
 }
 
 exec {'create_symlink':
-  command  => "sudo ln -sf /data/web_static/releases/test/ /data/web_static/current",
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
   provider => shell
 }
 
 exec {'change_ownership':
-  command  => "sudo chown -hR ubuntu:ubuntu /data/",
+  command  => 'sudo chown -hR ubuntu:ubuntu /data/',
   provider => shell
 }
 
@@ -28,6 +28,6 @@ exec {'add_line':
 }
 
 exec {'restart_nginx':
-  command  => "sudo service nginx restart",
+  command  => 'sudo service nginx restart',
   provider => shell
 }
