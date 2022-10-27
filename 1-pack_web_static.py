e9eae36cc85bbdcc9108e233809eee551d8bc5af
FrankDonny <arkohfrank1@gmail.com>
2022-10-27 03:06:00 +0000

    0x03. AirBnB clone - Deploy static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create archive module"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    new_date = datetime.now()
+    new_date = new_date.strftime('%Y%m%d%H%M%S')
+    local('mkdir -p versions/')
+    var = local(f'tar -cvf versions/web_static_{new_date}.tgz web_static/')
+
+    if var.succeeded:
+        return 'versions/web_static_' + new_date + '.tgz'
+    return None
